858345f15fcbfd0538c0002c5fe25fdd2dd00806
swetha12404 <swethamurugan242004@gmail.com>
2025-04-02 15:56:45 +0530

    initial commit

---
+++ src/main/java/com/jenkin_upload/Jenkin_work/App.java
@@ -0,0 +1,13 @@
+package com.jenkin_upload.Jenkin_work;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/jenkin_upload/Jenkin_work/Demo.java
@@ -0,0 +1,7 @@
+package com.jenkin_upload.Jenkin_work;
+
+public class Demo {
+	public static void main(String [] args) {
+		System.out.println("This file is successfully build in Jenkin");
+	}
+}
